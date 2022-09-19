6ddf6b8aafae4d94bc3acf2b5914dd928b622957
davAr1309 <david.alvarez@artificialy.com>
2022-09-19 11:12:19 +0200

    Using .to device in model_building.py

--- model_building.py
+++ model_building.py
@@ -40,7 +40,7 @@ class I2P(nn.Module):
 		if 'mobilenet_v2' in self.args.arch:
 			self.backbone = getattr(mobilenetv2_backbone, args.arch)(pretrained=False)
 		elif 'mobilenet' in self.args.arch:
-			self.backbone = getattr(mobilenetv1_backbone, args.arch)()		
+			self.backbone = getattr(mobilenetv1_backbone, args.arch)()
 		elif 'resnet' in self.args.arch:
 			self.backbone = getattr(resnet_backbone, args.arch)(pretrained=False)
 		elif 'ghostnet' in self.args.arch:
@@ -53,7 +53,7 @@ class I2P(nn.Module):
 	def forward(self,input, target):
 		"""Training time forward"""
 		_3D_attr, avgpool = self.backbone(input)
-		_3D_attr_GT = target.type(torch.cuda.FloatTensor)
+		_3D_attr_GT = target.type(torch.FloatTensor).to(self.args.device)
 		return _3D_attr, _3D_attr_GT, avgpool
 
 	def forward_test(self, input):
@@ -65,8 +65,12 @@ class I2P(nn.Module):
 class SynergyNet(nn.Module):
 	def __init__(self, args):
 		super(SynergyNet, self).__init__()
-		self.triangles = sio.loadmat('./3dmm_data/tri.mat')['tri'] -1
-		self.triangles = torch.Tensor(self.triangles.astype(np.int)).long().cuda()
+		# self.triangles = sio.loadmat('./3dmm_data/tri.mat')['tri'] -1
+		self.triangles = sio.loadmat('./train/SynergyNet/3dmm_data/tri.mat')['tri'] -1
+		if args.use_cuda:
+			self.triangles = torch.Tensor(self.triangles.astype(np.int)).long().cuda()
+		else:
+			self.triangles = torch.Tensor(self.triangles.astype(np.int)).long()
 		self.img_size = args.img_size
 		# Image-to-parameter
 		self.I2P = I2P(args)
@@ -76,7 +80,7 @@ class SynergyNet(nn.Module):
 		self.reverseDirection = MLP_rev(68)
 		self.LMKLoss_3D = WingLoss()
 		self.ParamLoss = ParamLoss()
-		
+
 		self.loss = {'loss_LMK_f0':0.0,
 					'loss_LMK_pointNet': 0.0,
 					'loss_Param_In':0.0,
@@ -84,23 +88,23 @@ class SynergyNet(nn.Module):
 					'loss_Param_S1S2': 0.0,
 					}
 
-		self.register_buffer('param_mean', torch.Tensor(param_pack.param_mean).cuda(non_blocking=True))
-		self.register_buffer('param_std', torch.Tensor(param_pack.param_std).cuda(non_blocking=True))
-		self.register_buffer('w_shp', torch.Tensor(param_pack.w_shp).cuda(non_blocking=True))
-		self.register_buffer('u', torch.Tensor(param_pack.u).cuda(non_blocking=True))
-		self.register_buffer('w_exp', torch.Tensor(param_pack.w_exp).cuda(non_blocking=True))
+		self.register_buffer('param_mean', torch.Tensor(param_pack.param_mean).to(args.device))
+		self.register_buffer('param_std', torch.Tensor(param_pack.param_std).to(args.device))
+		self.register_buffer('w_shp', torch.Tensor(param_pack.w_shp).to(args.device))
+		self.register_buffer('u', torch.Tensor(param_pack.u).to(args.device))
+		self.register_buffer('w_exp', torch.Tensor(param_pack.w_exp).to(args.device))
 
 		# If doing only offline evaluation, use these
-		# self.u_base = torch.Tensor(param_pack.u_base).cuda(non_blocking=True)
-		# self.w_shp_base = torch.Tensor(param_pack.w_shp_base).cuda(non_blocking=True)
-		# self.w_exp_base = torch.Tensor(param_pack.w_exp_base).cuda(non_blocking=True)
+		# self.u_base = torch.Tensor(param_pack.u_base).to(args.device)
+		# self.w_shp_base = torch.Tensor(param_pack.w_shp_base).to(args.device)
+		# self.w_exp_base = torch.Tensor(param_pack.w_exp_base).to(args.device)
 
 		# Online training needs these to parallel
-		self.register_buffer('u_base', torch.Tensor(param_pack.u_base).cuda(non_blocking=True))
-		self.register_buffer('w_shp_base', torch.Tensor(param_pack.w_shp_base).cuda(non_blocking=True))
-		self.register_buffer('w_exp_base', torch.Tensor(param_pack.w_exp_base).cuda(non_blocking=True))
+		self.register_buffer('u_base', torch.Tensor(param_pack.u_base).to(args.device))
+		self.register_buffer('w_shp_base', torch.Tensor(param_pack.w_shp_base).to(args.device))
+		self.register_buffer('w_exp_base', torch.Tensor(param_pack.w_exp_base).to(args.device))
 		self.keypoints = torch.Tensor(param_pack.keypoints).long()
- 
+
 		self.data_param = [self.param_mean, self.param_std, self.w_shp_base, self.u_base, self.w_exp_base]
 
 	def reconstruct_vertex_62(self, param, whitening=True, dense=False, transform=True, lmk_pts=68):
@@ -121,10 +125,10 @@ class SynergyNet(nn.Module):
 		p, offset, alpha_shp, alpha_exp = parse_param_62(param_)
 
 		if dense:
-			
+
 			vertex = p @ (self.u + self.w_shp @ alpha_shp + self.w_exp @ alpha_exp).contiguous().view(-1, 53215, 3).transpose(1,2) + offset
-			
-			if transform: 
+
+			if transform:
 				# transform to image coordinate space
 				vertex[:, 1, :] = param_pack.std_size + 1 - vertex[:, 1, :]
 
@@ -132,7 +136,7 @@ class SynergyNet(nn.Module):
 			"""For 68 pts"""
 			vertex = p @ (self.u_base + self.w_shp_base @ alpha_shp + self.w_exp_base @ alpha_exp).contiguous().view(-1, lmk_pts, 3).transpose(1,2) + offset
 
-			if transform: 
+			if transform:
 				# transform to image coordinate space
 				vertex[:, 1, :] = param_pack.std_size + 1 - vertex[:, 1, :]
 
@@ -140,10 +144,10 @@ class SynergyNet(nn.Module):
 
 	def forward(self, input, target):
 		_3D_attr, _3D_attr_GT, avgpool = self.I2P(input, target)
-			
+
 		vertex_lmk = self.reconstruct_vertex_62(_3D_attr, dense=False)
 		vertex_GT_lmk = self.reconstruct_vertex_62(_3D_attr_GT, dense=False)
-		self.loss['loss_LMK_f0'] = 0.05 *self.LMKLoss_3D(vertex_lmk, vertex_GT_lmk, kp=True)		
+		self.loss['loss_LMK_f0'] = 0.05 *self.LMKLoss_3D(vertex_lmk, vertex_GT_lmk, kp=True)
 		self.loss['loss_Param_In'] = 0.02 * self.ParamLoss(_3D_attr, _3D_attr_GT)
 
 		point_residual = self.forwardDirection(vertex_lmk, avgpool, _3D_attr[:,12:52], _3D_attr[:,52:62])
@@ -183,7 +187,7 @@ class WrapUpSynergyNet(nn.Module):
 		self.reverseDirection = MLP_rev(68)
 		self.LMKLoss_3D = WingLoss()
 		self.ParamLoss = ParamLoss()
-		
+
 		self.loss = {'loss_LMK_f0':0.0,
 					'loss_LMK_pointNet': 0.0,
 					'loss_Param_In':0.0,
@@ -202,7 +206,7 @@ class WrapUpSynergyNet(nn.Module):
 		self.register_buffer('w_shp_base', torch.Tensor(param_pack.w_shp_base))
 		self.register_buffer('w_exp_base', torch.Tensor(param_pack.w_exp_base))
 		self.keypoints = torch.Tensor(param_pack.keypoints).long()
- 
+
 		self.data_param = [self.param_mean, self.param_std, self.w_shp_base, self.u_base, self.w_exp_base]
 
 		try:
@@ -230,10 +234,10 @@ class WrapUpSynergyNet(nn.Module):
 		p, offset, alpha_shp, alpha_exp = parse_param_62(param_)
 
 		if dense:
-			
+
 			vertex = p @ (self.u + self.w_shp @ alpha_shp + self.w_exp @ alpha_exp).contiguous().view(-1, 53215, 3).transpose(1,2) + offset
-			
-			if transform: 
+
+			if transform:
 				# transform to image coordinate space
 				vertex[:, 1, :] = param_pack.std_size + 1 - vertex[:, 1, :]
 
@@ -241,7 +245,7 @@ class WrapUpSynergyNet(nn.Module):
 			"""For 68 pts"""
 			vertex = p @ (self.u_base + self.w_shp_base @ alpha_shp + self.w_exp_base @ alpha_exp).contiguous().view(-1, lmk_pts, 3).transpose(1,2) + offset
 
-			if transform: 
+			if transform:
 				# transform to image coordinate space
 				vertex[:, 1, :] = param_pack.std_size + 1 - vertex[:, 1, :]
 
@@ -292,7 +296,7 @@ class WrapUpSynergyNet(nn.Module):
 
 			with torch.no_grad():
 				param = self.forward_test(img)
-			
+
 			param = param.squeeze().cpu().numpy().flatten().astype(np.float32)
 
 			lmks = predict_sparseVert(param, roi_box, transform=True)
